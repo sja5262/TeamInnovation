6e2a4e1a8a225aa4184fc1d0dd52cfb0b1ba89ae
sja5262 <32081041+sja5262@users.noreply.github.com>
2017-11-01 19:22:13 -0400

    Update Caesar.java
    still under construction

--- src/Cipher/Caesar.java
+++ src/Cipher/Caesar.java
@@ -1,22 +1,71 @@
 package Cipher;
 
+import java.util.Locale;
 import javax.swing.*;
-//import com.google.cloud.translate.Translate;
-//import com.google.cloud.translate.Translate.TranslateOption;
-//import com.google.cloud.translate.TranslateOptions;
-//import com.google.cloud.translate.Translation;
 
 public class Caesar {
 
     public String decryptCaesar(String informedcode) {
-        String message, decryptedMessage = "";
+        String message, decryptedMessage = "", transMessage, finalTrans, check;
         int key;
         char character;
+        GoogleTranslate trans;
+        String language;
         JTextArea allResults;
 
         message = informedcode;
         allResults = new JTextArea("");
+        trans = new GoogleTranslate();
+        language = new String("");
+        transMessage = new String("");
+        String langName = new String("");
+        StringBuilder resultPool = new StringBuilder("");
+        finalTrans = new String("");
+        check = new String("");
+        
+        
+        language = trans.detectLanguage(message);
+            
+            Locale loc = new Locale(language);
+            langName = loc.getDisplayLanguage(loc);
+            
+           if ("English".equals(langName))
+           {
+               String[] splitMessage = message.split("\\s+");
+                System.out.println(splitMessage.length);
+            
+                int k = 0;
+            
+                for (int i = 0; i < splitMessage.length; i++) {
+                
+                check = trans.checkEnglish(finalTrans);
+                System.out.println(check);
+                resultPool.append(transMessage+" ");
+                k++;
+                }
+                             
+               finalTrans = message.toUpperCase();
+           }
+           else
+           {
+            String[] splitMessage = message.split("\\s+");
+            System.out.println(splitMessage.length);
+            
+            int k = 0;
+            
+            for (int i = 0; i < splitMessage.length; i++) {
+                
+                transMessage = trans.translateLanguage(splitMessage[k], language);
+                resultPool.append(transMessage+" ");
+                k++;
+                }
+            
+            finalTrans = resultPool.toString();
+            finalTrans = finalTrans.toUpperCase();
+            }
 
+        allResults.append("Language: "+ langName +" - Message: "+finalTrans);
+/*
         for (key = 1; key < 27; ++key) {
             decryptedMessage = "";
             for (int i = 0; i < message.length(); ++i) {
@@ -38,22 +87,15 @@ public class Caesar {
                     decryptedMessage += character;
                 } else {
                     decryptedMessage += character;
-                }
-                //Translate translate = createTranslateService();
-                //List<Detection> detections = translate.detect(ImmutableList.of(sourceText));
-                //System.out.println("Langauge(s) detected:");
-                //for (Detection detection : detections){
-                //System.out.println("\t%s\n", dections;
-                //Scanner scan = new Scanner(System.in);  
-                //String word = scan.nextLine();  
-                //checkEnglish checkEng = new checkEnglish();  
-                //if(checkEng.wordCheck(word)){  
+                } 
+                language = trans.detectLanguage(decryptedMessage);
 
             }
             allResults.append("Caesar Key " + key + ":\n" + decryptedMessage + "\n");
         }
 //}
         message = allResults.getText();
-        return allResults.getText();
+       */ return allResults.getText();
     }
+    
 }
